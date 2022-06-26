e7a607f23ee1f2523b57dbbca3a82eeff3b36cae
Name:Marcial <92460732+DocMarcial@users.noreply.github.com>
2022-06-25 21:17:31 -0600

    Added comments.

--- EI_portenta_h7_microphone_continuous_M4/EI_portenta_h7_microphone_continuous_M4.ino
+++ EI_portenta_h7_microphone_continuous_M4/EI_portenta_h7_microphone_continuous_M4.ino
@@ -20,12 +20,22 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    *
  * SOFTWARE.                                                                        *
  ************************************************************************************
+ * The MIT License (MIT)                                                            *
+ *                                                                                  *
  * Modifications to the Software "portenta_h7_microphone_continuous.ino"            *
  * and saved herin as the Software "EI_portenta_h7_microphone_continuous_M4.ino"    *
- * Copyright (c) 2022 Mr. Marcial                                                   *
+ * Copyright (c) 2022 M. Marcial                                                    *
  *                                                                                  *
  * The above copyright notice shall be included in                                  *    
  * all copies or substantial portions of the Software.                              *
+ *                                                                                  *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    *
+ * SOFTWARE.                                                                        *
  ************************************************************************************
  */
 
